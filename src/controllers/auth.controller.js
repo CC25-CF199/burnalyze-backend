cb591a553bf56b130553dca560e68e3cf2968553
Achmad Akif <achmadakif7@gmail.com>
2025-05-21 16:07:24 +0700

    feat(Auth): [BBE-2] create user registration and login validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,42 @@
-const { userService } = require('../services');
+const UserModel = require('../models/user.model');
+const { signUpSchema } = require('../validations/auth.validation');
 
+// TO-DO improve error handling
 const register = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { error, value } = signUpSchema.validate(req.body, {
+      abortEarly: false,
+    });
 
-  const user = await userService.getUserByEmail(email);
+    if (error) {
+      console.error(error);
+      return res.status(500).json(error.details);
+    }
 
-  if (user) {
-    return res.status(400).json({ message: 'Email already registered.' });
+    const isEmailTaken = await UserModel.getUserByEmail(value.email);
+    const isUsernameTaken = await UserModel.getUserByUsername(value.username);
+
+    if (isEmailTaken.data.length > 0) {
+      return res.status(400).json({ message: 'Email already registered.' });
+    } else if (isUsernameTaken.data.length > 0) {
+      return res
+        .status(400)
+        .json({ message: 'Username is taken or already registered' });
+    }
+
+    const createdUser = await UserModel.createUser(value);
+    return res
+      .status(201)
+      .json({ ...createdUser, message: 'Account successfully registered.' });
+  } catch (error) {
+    console.error('Unexpected Error:', error.message);
+    return res.status(500).json({ message: error.message });
   }
-
-  return res.status(200).json({ message: 'Successfully registered.' });
 };
+
+const login = async (req, res) => {};
 
 module.exports = {
   register,
+  login,
 };
