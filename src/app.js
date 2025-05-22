7997c2ed31817b5d2127f73f0e6914f1d49c3bc2
Achmad Akif <achmadakif7@gmail.com>
2025-05-22 19:13:18 +0700

    feat(Register): [BBE-2] implement cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const createError = require('http-errors');
+const cors = require('cors');
+
 const { errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
+
+app.use(cors());
 
 require('dotenv').config();
 
@@ -17,6 +21,7 @@ app.use((req, res, next) => {
   next(createError(404, 'Not Found'));
 });
 
+// Error handler middleware
 app.use(errorHandler);
 
 app.listen(PORT, () => {
