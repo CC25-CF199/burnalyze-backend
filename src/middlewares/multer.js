998acfb4437e59b0a61f15919737bd4a56f01553
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2025-06-09 16:13:44 +0700

    feat(upload): create upload directory and update multer storage path

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -4,7 +4,8 @@ const createError = require('http-errors');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'upload/');
+    const uploadPath = path.join(__dirname, '../../upload');
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
